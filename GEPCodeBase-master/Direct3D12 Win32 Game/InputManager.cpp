41d79100539ed0977fa0ee33b7266624faf8e648
Elliotmartin97 <Elliotmartin97@outlook.com>
2018-02-16 07:50:02 +0000

    requires work for gamepad to function with data structure

--- GEPCodeBase-master/Direct3D12 Win32 Game/InputManager.cpp
+++ GEPCodeBase-master/Direct3D12 Win32 Game/InputManager.cpp
@@ -11,10 +11,23 @@ void InputManager::LoadInputSettings()
 
 void InputManager::UpdateInput()
 {
-	DirectX::Keyboard::State     keyState;
-	keyState = m_keyboard->GetState();
+	DirectX::Keyboard::State key_state;
+	//Gamepad state and gamepad button state tracker have different methods, there is no iskeydown for gamepad
+	//DirectX::GamePad::State controller_state;
+	DirectX::GamePad::ButtonStateTracker controller_state;
+	key_state = m_keyboard->GetState();
+	//getstate() needs a number for the player
+
+	//controller_state = m_gamePad->GetState(0);
+
+	//keyboard
 	for (int i = 0; i < input_data_list.size(); i++) 
 	{
-		input_data_list[i].pressed = keyState.IsKeyDown(input_data_list[i].keys);
+		input_data_list[i].pressed = key_state.IsKeyDown(input_data_list[i].keys);
+	}
+	//gamepad
+	for (int i = 0; i < input_data_list.size(); i++)
+	{
+		//input_data_list[i].pressed = controller_state.
 	}
 }
