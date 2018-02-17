8ae1a0b03413d4a2a7e9bc50b510789e59af6806
Jack Watson <uohane@gmail.com>
2018-02-17 11:52:46 +0000

    Fixed jumping, much faster and responsive.

--- GEPCodeBase-master/Direct3D12 Win32 Game/Player2D.cpp
+++ GEPCodeBase-master/Direct3D12 Win32 Game/Player2D.cpp
@@ -15,35 +15,19 @@ Player2D::~Player2D()
 void Player2D::Tick(GameStateData * _GSD)
 {
 
-	setGravity(1000.0f);
+	if (game_states == GROUNDED)
+	{
+		setGravity(0.0f);
+		if (_GSD->m_keyboardState.Space)
+		{
+			setGravity(1000.0f);
+			game_states = JUMPING;
+			AddForce(-jump_force * Vector2::UnitY);
+		}
+	}
 	AddForce(gravity*Vector2::UnitY);
-	int game_state = GROUNDED;
-	switch (game_state)
-	{
-	case GROUNDED:
-	{
-		setGravity(0.0f);
-		break;
-	}
-	case JUMPING:
-	{
-		setGravity(0.0f);
-		break;
-	}
-
-	case FALLING:
-	{
-		setGravity(1000.0f);
-		break;
-	}
-	}
-
 //Push the guy around in the directions for the key presses
-	if (_GSD->m_keyboardState.Space)
-	{
-		game_state = JUMPING;
-		AddForce(-jump_force * Vector2::UnitY);	
-	}
+
 	if (_GSD->m_keyboardState.A)
 	{
 		AddForce(-m_drive * Vector2::UnitX);
@@ -53,9 +37,6 @@ void Player2D::Tick(GameStateData * _GSD)
 		AddForce(m_drive * Vector2::UnitX);
 	}
 
-	Vector2 mousePush = Vector2(_GSD->m_mouseState.x, _GSD->m_mouseState.y);
-	
-	AddForce(m_drive*mousePush);
 	
 //GEP:: Lets go up the inheritence and share our functionality
 
@@ -67,20 +48,21 @@ void Player2D::Tick(GameStateData * _GSD)
 		m_pos.x = 1.0f;
 		
 	}
-	if (m_pos.y < 0.0f)
+	if (m_pos.y <= 0.0f)
 	{
-		m_pos.y = 1.0f;
-		game_state = GROUNDED;
+		m_pos.y = 0.0f;
+		
 	}
 
 	if (m_pos.x > m_limit.x)
 	{
-		m_pos.x =  m_limit.x - 2.0f;
+		m_pos.x =  m_limit.x- 2.0f;
 		
 	}
-	if (m_pos.y > m_limit.y)
+	if (m_pos.y >= m_limit.y)
 	{
 		m_pos.y = m_limit.y - 1.0f;	
+		game_states = GROUNDED;
 	}
 	
 }
