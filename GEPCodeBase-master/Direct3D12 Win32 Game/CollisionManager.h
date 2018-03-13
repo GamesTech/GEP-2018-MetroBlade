e1c5bc8d92767df1717281606d47fb45cb468b22
Jack Watson <uohane@gmail.com>
2018-03-13 14:59:26 +0000

    Refined the collision system. Collisions only check both rects now. Bounding walls were removed and the if statement and other functions were removed to make the class more clean. There is a bug where sometimes players stick together, will look into it after a punching collider system is completed.

--- GEPCodeBase-master/Direct3D12 Win32 Game/CollisionManager.h
+++ GEPCodeBase-master/Direct3D12 Win32 Game/CollisionManager.h
@@ -5,10 +5,6 @@ class CollisionManager
 {
 private:
 	std::vector<Collider> colliders;
-	Vector2 left;
-	Vector2 right;
-	Vector2 top;
-	Vector2 bottom;
 public:
 	CollisionManager() = default;
 	~CollisionManager() = default;
@@ -16,9 +12,7 @@ public:
 	void init();
 
 	void addCollider(Collider collider);
-	bool boundingBox(Vector2 wallType, int rect2ID);
 	void updateColliders(Vector2 position, int id);
-	void updateBoundingBox(int id);
 	void update();
 	bool checkCollisions(int id);
 	Vector2 collide();
