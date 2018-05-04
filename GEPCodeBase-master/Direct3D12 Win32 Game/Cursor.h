3214b0e7bbf0727a99afb94203b760bc4d422d1b
Jack Watson <uohane@gmail.com>
2018-05-04 16:41:59 +0100

    Commiting cursor related includes and filters that were forgotten
    This commit also includes the addition of doxygen commenting in the cursor header.

--- GEPCodeBase-master/Direct3D12 Win32 Game/Cursor.h
+++ GEPCodeBase-master/Direct3D12 Win32 Game/Cursor.h
@@ -1,3 +1,21 @@
+/**
+*  @file    UICanvas.h
+*  @author  Nathan Butt (GitHub - n86-64)
+*  @date    12/03/2018
+*  @version v1.0
+*
+*  @section System Module
+*           UI Renderer
+*
+*  @brief Defines a UI object.
+*
+*  @section Description
+*
+*	A UI Object is any object that is managed by the UICanvas and is being used to display information on screen.
+*   It takes the form of a modified game object.
+*
+*/
+
 #pragma once
 #include "UIObject.h"
 #include "InputManager.h"
